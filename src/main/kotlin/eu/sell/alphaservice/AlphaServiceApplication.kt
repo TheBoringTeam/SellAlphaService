5899c22213f4b269bb4bc55fe35a6adc79b6891e
HolkerDev <nick.penkovskiy@gmail.com>
2020-10-19 23:10:16 +0200

    add init project

---
+++ src/main/kotlin/eu/sell/alphaservice/AlphaServiceApplication.kt
@@ -0,0 +1,11 @@
+package eu.sell.alphaservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlphaServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlphaServiceApplication>(*args)
+}
---
+++ src/test/kotlin/eu/sell/alphaservice/AlphaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package eu.sell.alphaservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlphaServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
